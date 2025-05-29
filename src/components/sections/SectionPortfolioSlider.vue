cc47be4de13c323bb48e2fb3bc41e937914501b8
ortadox <misha.vidiakin@gmail.com>
2025-05-29 19:18:42 +0300

    swiper

--- src/components/sections/SectionPortfolioSlider.vue
+++ src/components/sections/SectionPortfolioSlider.vue
@@ -23,34 +23,76 @@ onMounted(async () => {
     swiperEl.centeredSlides = true
     swiperEl.loop = true
     swiperEl.loopedSlides = portfolio.value.length
-    swiperEl.spaceBetween = 30
+    swiperEl.spaceBetween = 60
     swiperEl.addEventListener('swiperinit', () => {
       swiperEl.swiper.slideToLoop(3, 0)
+    })
+
+    const nextBtn = document.querySelector('.swiper-button-next')
+    const prevBtn = document.querySelector('.swiper-button-prev')
+
+    nextBtn?.addEventListener('click', () => {
+      swiperEl.swiper.slideNext()
+    })
+
+    prevBtn?.addEventListener('click', () => {
+      swiperEl.swiper.slidePrev()
     })
   }
 })
 </script>
 
 <template>
-  <section class="portfolio-slider box-y">
+  <section class="portfolio-slider box-y pr">
     <div v-if="!isLoading" style="width: 110%; margin: auto">
       <swiper-container>
-        <swiper-slide v-for="(value, key) in portfolio" :key="key">
-          <img :src="`${API_URL}image/${value.image_id}`" alt="" />
+        <swiper-slide style="padding: 2rem 0" v-for="(value, key) in portfolio" :key="key">
+          <img class="portfolio-slider__img" :src="`${API_URL}image/${value.image_id}`" alt="" />
         </swiper-slide>
       </swiper-container>
+    </div>
+    <div class="swiper-buttons__wrapper pa center">
+      <div class="box-x swiper-buttons">
+        <button class="swiper-button swiper-button-prev">
+          <img src="@/assets/icons/arrow_left.svg" alt="arrow_left" />
+        </button>
+        <button class="swiper-button swiper-button-next">
+          <img src="@/assets/icons/arrow_right.svg" alt="arrow_right" />
+        </button>
+      </div>
     </div>
   </section>
 </template>
 
 <style scoped lang="sass">
+.swiper-button
+  pointer-events: auto
+.swiper-buttons
+  width: 42%
+  &__wrapper
+    width: 110%
+    +h
+    z-index: 2
+    pointer-events: none
+  & img
+    width: 8rem
+    height: 8rem
+    padding: 0rem
+    background-color: #000
+swiper-slide.swiper-slide-active
+  position: relative
+  z-index: 1
+  & img
+      transform: scale(1.1)
+      transition: .2s
+
 .portfolio-slider
   margin: 0 0 0 -10%
   width: 110%
   overflow: hidden
   align-items: center
   justify-content: center
-  & img
+  &__img
     width: 100%
     height: 300px
     object-fit: cover
