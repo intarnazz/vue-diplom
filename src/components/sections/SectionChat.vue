d829a7b0e1001081ca52b44bd0cbcec738a909dd
Ortadox <misha.vidiakin@gmail.com>
2025-06-26 10:44:31 +0300

    char form

--- src/components/sections/SectionChat.vue
+++ src/components/sections/SectionChat.vue
@@ -13,8 +13,6 @@ const chat = ref()
 async function down() {
   await nextTick()
   chat.value.scrollTop = chat.value.scrollHeight
-  console.log(chat.value.scrollTop)
-  console.log(chat.value.scrollHeight)
 }
 
 function chatMessageSentEve(data) {
@@ -68,20 +66,26 @@ watch(() => messages, down, { deep: true })
     <img class="img pa" src="@/assets/img/cyg23qqdamt41.jpg" alt="reg-bg" />
     <div class="chat_wrapper pr box-y wh p2 gap2">
       <div class="flex"></div>
-      <div ref="chat" class="chat__list box-y pa" v-if="user">
+      <div ref="chat" class="chat__list wh box-y pa" v-if="user">
         <div class="flex"></div>
         <div v-for="(message, key) in messages" :key="key" class="box-x">
           <template v-if="message.user_id">
             <ComponentMessage :message="message" :user="user" />
           </template>
         </div>
+        <form @submit.prevent="send" class="chat__message-bar-wrapper box-x">
+          <h1>{{ isPageAtTop }}</h1>
+          <input
+            v-model="text"
+            placeholder="Сообщение..."
+            class="chat__message-bar wh"
+            type="text"
+          />
+          <button class="box-x" type="submit">
+            <img src="@/assets/icons/send.svg" alt="send" />
+          </button>
+        </form>
       </div>
-      <form @submit.prevent="send" class="chat__message-bar-wrapper box-x">
-        <input v-model="text" placeholder="Сообщение..." class="chat__message-bar wh" type="text" />
-        <button class="box-x" type="submit">
-          <img src="@/assets/icons/send.svg" alt="send" />
-        </button>
-      </form>
     </div>
   </section>
 </template>
@@ -94,8 +98,7 @@ watch(() => messages, down, { deep: true })
     position: relative
   &__list
     overflow-y: scroll
-    padding: 0 2rem
-    height: 91%
+    padding: 0 2rem 1rem
   & img
     opacity: 30%
   &__message-bar-wrapper
