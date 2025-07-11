3a2dade4023f1e4cca044528bb2a5a95af771e83
Ortadox <misha.vidiakin@gmail.com>
2025-07-11 16:16:26 +0300

    NEE HEADER CSS TW

--- src/layout/header/HeaderMain.vue
+++ src/layout/header/HeaderMain.vue
@@ -15,47 +15,15 @@ import ComponentLogo from '@/components/ComponentLogo.vue'
 // })
 </script>
 
+
+
+
 <template>
   <header class="box-x top header">
-    <ComponentLogo />
-    <nav class="header__nav box-x top">
-      <div class="box-y header__nav-sub-menu-wrapper header__nav-sub-menu-wrapper_1">
-        <button class="">О нас</button>
-        <div class="box-y header__nav-sub-menu header__nav-sub-menu_1">
-          <div class="box-x">О нас подробнее</div>
-          <div class="box-x">О нас подробнее2</div>
-        </div>
-      </div>
-      <div class="box-y header__nav-sub-menu-wrapper header__nav-sub-menu-wrapper_2">
-        <button class="">О вас</button>
-        <div class="box-y header__nav-sub-menu header__nav-sub-menu_2">
-          <div class="box-x">О вас подробнее</div>
-          <div class="box-x">О вас подробнее2</div>
-        </div>
-      </div>
-    </nav>
+
   </header>
 </template>
 
 <style lang="sass" scoped>
-.header
-  padding: 1rem 2rem
-  &__nav
-    background-color: #fff
-    box-shadow: 0 0 10px #000
-    border-radius: 20px
-    & > .header__nav-sub-menu-wrapper
-      > button
-        padding: 1rem 0rem 1rem 2rem
-      .header__nav-sub-menu
-        padding: 1rem 2rem
-        display: none
-        max-width: 0px
-        max-height: 0px
-        position: absolute
-        cursor: pointer
-        &:hover
-          display: flex
-      &:hover .header__nav-sub-menu
-        display: flex
+
 </style>
