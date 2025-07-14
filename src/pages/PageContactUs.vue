e54804483a381474eeb7b999a775add98b1f3a65
Ortadox <misha.vidiakin@gmail.com>
2025-07-14 12:03:24 +0300

    ContactUsMain

--- src/pages/PageContactUs.vue
+++ src/pages/PageContactUs.vue
@@ -2,7 +2,7 @@
 import FooterMain from '@/layout/footer/FooterMain.vue'
 import LayoutPage from '@/layout/LayoutPage.vue'
 import LayoutOpener from '@/layout/LayoutOpener.vue'
-import SectionContactUs from '@/components/sections/SectionContactUs.vue'
+import SectionContactUs from '@/components/sections/contactUs/ContactUsMain.vue'
 </script>
 
 <template>
