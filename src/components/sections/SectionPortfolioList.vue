f33cdbbe57e2fc713a6ad95b73de9936774303d6
Ortadox <misha.vidiakin@gmail.com>
2025-07-16 00:47:23 +0300

    пятно

--- src/components/sections/SectionPortfolioList.vue
+++ src/components/sections/SectionPortfolioList.vue
@@ -95,9 +95,6 @@ onUnmounted(() => {
       </LayoutWrapper>
     </div>
     <LayoutWrapper class="box-y gap2">
-      <div class="absolute">
-        <img src="@/assets/img/Rectangle 153.png" class="opacity-50" alt="" />
-      </div>
       <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         <RouterLink
           v-for="portfolio in portfolioList"
