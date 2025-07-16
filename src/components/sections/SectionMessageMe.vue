a3f32263e132d8225c9a511302596199a91f4175
Ortadox <misha.vidiakin@gmail.com>
2025-07-16 04:42:08 +0300

    message me

--- src/components/sections/SectionMessageMe.vue
+++ src/components/sections/SectionMessageMe.vue
@@ -1,22 +1,31 @@
 <script setup>
 import FormGuestQuery from '@/components/form/FormGuestQuery.vue'
+import LayoutOpener from '@/layout/LayoutOpener.vue'
 </script>
 
 <template>
-  <section class="login w box-x h-screen">
-    <div class="wh flex9 flex media">
-      <img class="img" src="@/assets/img/prateek-katyal-bQIgHc6AHUI-unsplash.webp" alt="login-bg" />
-    </div>
-    <div class="login__wrapper wh box-y gap p5 flex16 flex">
-      <div class="header-spase"></div>
-      <h2>Message Me</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora dolores voluptatem
-        expedita! Provident odio possimus unde labore hic nisi optio nesciunt alias! Est accusamus
-        quaerat soluta consectetur aperiam fugiat laboriosam.
-      </p>
-      <FormGuestQuery class="flex" />
-    </div>
+  <section>
+    <LayoutOpener class="login box-x as">
+      <div class="box-y flex9">
+        <div class="pr flex media">
+          <img
+            class="img pa"
+            src="@/assets/img/prateek-katyal-bQIgHc6AHUI-unsplash.webp"
+            alt="login-bg"
+          />
+        </div>
+      </div>
+      <div class="login__wrapper wh box-y gap p5 flex16 flex">
+        <div class="header-spase"></div>
+        <h2>Message Me</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora dolores voluptatem
+          expedita! Provident odio possimus unde labore hic nisi optio nesciunt alias! Est accusamus
+          quaerat soluta consectetur aperiam fugiat laboriosam.
+        </p>
+        <FormGuestQuery class="flex" />
+      </div>
+    </LayoutOpener>
   </section>
 </template>
 
--- src/pages/PageMessageMe.vue
+++ src/pages/PageMessageMe.vue
@@ -1,15 +1,12 @@
 <script setup>
 import FooterMain from '@/layout/footer/FooterMain.vue'
 import LayoutPage from '@/layout/LayoutPage.vue'
-import ComponentOpener from '@/layout/LayoutOpener.vue'
 import SectionMessageMe from '@/components/sections/SectionMessageMe.vue'
 </script>
 
 <template>
   <LayoutPage class="main">
-    <ComponentOpener class="box-y gap2">
-      <SectionMessageMe />
-    </ComponentOpener>
+    <SectionMessageMe />
   </LayoutPage>
   <FooterMain />
 </template>
