becac00434e2bd0a609b78ffc0df66ef799bff3d
ortadox <misha.vidiakin@gmail.com>
2025-07-19 16:26:20 +0300

    message 9/16

--- src/pages/PageMessageMe.vue
+++ src/pages/PageMessageMe.vue
@@ -1,12 +1,24 @@
 <script setup>
 import FooterMain from '@/layout/footer/FooterMain.vue'
 import LayoutPage from '@/layout/LayoutPage.vue'
+import Layout9By16 from '@/layout/Layout9By16.vue'
 import SectionMessageMe from '@/components/sections/SectionMessageMe.vue'
 </script>
 
 <template>
   <LayoutPage class="main">
-    <SectionMessageMe />
+    <Layout9By16>
+      <template #image>
+        <img
+          class="img pa"
+          src="@/assets/img/prateek-katyal-bQIgHc6AHUI-unsplash.webp"
+          alt="login-bg"
+        />
+      </template>
+      <template #default>
+        <SectionMessageMe />
+      </template>
+    </Layout9By16>
   </LayoutPage>
   <FooterMain />
 </template>
