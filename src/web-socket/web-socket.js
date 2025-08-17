7ee90d7ff7eaca2f125e52a5d402cf111ffd05b5
Ortadox <misha.vidiakin@gmail.com>
2025-08-17 07:28:50 +0300

    build

--- src/web-socket/web-socket.js
+++ src/web-socket/web-socket.js
@@ -5,14 +5,18 @@ import Pusher from 'pusher-js'
 export class WebSocket {
   constructor() {
     const user = User()
+
+    // Enable pusher logging - don't include this in production
+    // Pusher.logToConsole = true
+
     window.Pusher = Pusher
     window.Echo = new Echo({
       broadcaster: 'pusher',
       key: import.meta.env.VITE_APP_WEBSOCKETS_KEY,
-      wsHost: import.meta.env.VITE_APP_WEBSOCKETS_SERVER,
-      wsPort: 6001,
-      cluster: 'mt1',
-      forceTLS: false,
+      cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+      // cluster: 'mt1',
+      forceTLS: true,
+      encrypted: true,
       disableStats: true,
       authEndpoint: `${import.meta.env.VITE_API_URL}broadcasting/auth`,
       auth: {
